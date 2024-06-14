65c40a3f65d12701e43860e7374114506ef65187
dengmingcong <mc.deng@outlook.com>
2024-06-14 09:34:16 +0800

    feat: add data provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Refine, WelcomePage } from "@refinedev/core";
 
+import { dataProvider } from "./providers/data-provider";
+
 function App() {
   return (
-    <Refine>
+    <Refine dataProvider={dataProvider}>
       <WelcomePage />
     </Refine>
   );
