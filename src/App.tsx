846c68e828055cd9867dd4889323f2708d53e588
dengmingcong <mc.deng@outlook.com>
2024-06-15 14:55:56 +0800

    feat: implement get list

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { dataProvider } from "./providers/data-provider";
 
 import { ShowProduct } from "./pages/products/show";
 import { EditProduct } from "./pages/products/edit";
+import { ListProducts } from "./pages/products/list";
 
 export default function App(): JSX.Element {
   return (
     <Refine dataProvider={dataProvider}>
       {/* <ShowProduct /> */}
-      <EditProduct />
+      {/* <EditProduct /> */}
+      <ListProducts />
     </Refine>
   );
 }
