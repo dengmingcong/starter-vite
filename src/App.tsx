29b2d8c90492a2a7a74fe21c2d91cbc3980ac7a4
dengmingcong <mc.deng@outlook.com>
2024-06-15 14:47:37 +0800

    feat: implement update one record

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Refine, WelcomePage } from "@refinedev/core";
+import { Refine } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
-import { ShowProduct } from "./pages/products/show";
 
-function App() {
+import { ShowProduct } from "./pages/products/show";
+import { EditProduct } from "./pages/products/edit";
+
+export default function App(): JSX.Element {
   return (
     <Refine dataProvider={dataProvider}>
-      <ShowProduct />
+      {/* <ShowProduct /> */}
+      <EditProduct />
     </Refine>
   );
 }
-
-export default App;
