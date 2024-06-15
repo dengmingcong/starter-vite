d22f4294a444da1dfca378a7daef54061522ef91
dengmingcong <mc.deng@outlook.com>
2024-06-15 15:24:50 +0800

    refactor: add table

--- src/pages/products/list.tsx
+++ src/pages/products/list.tsx
@@ -1,12 +1,13 @@
-import { useList } from "@refinedev/core";
+import { useTable } from "@refinedev/core";
 
 export const ListProducts = () => {
-    const { data, isLoading } = useList({
-        resource: "products",
-        pagination: { current: 1, pageSize: 10 },
-        sorters: [{ field: "name", order: "asc" }],
-        filters: [{ field: "material", operator: "eq", value: "Aluminum" }],
-      });
+  const {
+    tableQueryResult: { data, isLoading },
+  } = useTable({
+    resource: "products",
+    pagination: { current: 1, pageSize: 10 },
+    sorters: { initial: [{ field: "id", order: "asc" }] },
+  });
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -15,19 +16,28 @@ export const ListProducts = () => {
   return (
     <div>
       <h1>Products</h1>
-      <ul>
-        {data?.data?.map((product) => (
-          <li key={product.id}>
-            <p>
-              {product.name}
-              <br />
-              Price: {product.price}
-              <br />
-              Material: {product.material}
-            </p>
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Material</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.data?.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.name}</td>
+              <td>{product.category?.id}</td>
+              <td>{product.material}</td>
+              <td>{product.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
